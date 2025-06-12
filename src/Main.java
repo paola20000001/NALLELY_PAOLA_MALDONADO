f6adce168be807610a5be9f400227458e9edfcc6
paola20000001 <Nallely.maldonado@udla.edu.ec>
2025-06-12 17:24:05 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,35 @@
 import figurasGeometricas.CirculoDibujable;
+import figurasGeometricas.CuadradoDibujable;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
 
-        figurasGeometricas.CirculoDibujable circulo = new CirculoDibujable( 100,  0,  0);
+        CirculoDibujable circulo = new CirculoDibujable(100, 50, 50);
+        CuadradoDibujable cuadrado = new CuadradoDibujable(100, 200, 50);
 
         System.out.println("El perímetro del círculo es: " + circulo.getPerimetro());
         System.out.println("El área del círculo es: " + circulo.getArea());
 
-        JFrame frame = new JFrame( "Dibujar círculo");
-        JPanel panel = new JPanel() {
+        System.out.println("El perímetro del cuadrado es: " + cuadrado.getPerimetro());
+        System.out.println("El área del cuadrado es: " + cuadrado.getArea());
 
+        JFrame frame = new JFrame("Dibujar figuras");
+        JPanel panel = new JPanel() {
             @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
                 circulo.dibujar2D(g);
+                cuadrado.dibujar2D(g);
             }
         };
-        panel.setPreferredSize(new Dimension(400,400));
+
+        panel.setPreferredSize(new Dimension(400, 400));
         frame.add(panel);
         frame.pack();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
-
     }
 }
