649933a4dc61dc1be637e25aece272e144232872
paola20000001 <nallelymaldonado@udla.edu.ec>
2025-06-12 16:58:51 -0500

    primero

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,25 @@
-
 import figurasGeometricas.CirculoDibujable;
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
 
-        figurasGeometricas.CirculoDibujable circulo = new CirculoDibujable(valor1: 100, x: 0, y: 0);
+        figurasGeometricas.CirculoDibujable circulo = new CirculoDibujable( 100,  0,  0);
 
         System.out.println("El perímetro del círculo es: " + circulo.getPerimetro());
         System.out.println("El área del círculo es: " + circulo.getArea());
 
-        JFrame frame = new JFrame();
+        JFrame frame = new JFrame( "Dibujar círculo");
+        JPanel panel = new JPanel() {
+
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                circulo.dibujar2D(g);
+            }
+        };
+
 
     }
 }
